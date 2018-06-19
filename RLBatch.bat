2cd08a9b8136629b74d982b9fcc2466f4dedd0ce
Johnny Duncan <johnny@gaylandwardseed.com>
2018-06-19 11:38:41 -0500

    added comments and display off to batch file

--- RLBatch.bat
+++ RLBatch.bat
@@ -1,12 +1,19 @@
 @echo off
-:: Created By: Johnny Duncan
-:: Last Modified: 6/18/18
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Batch file to run a java program redlionexec.jar on mon-fri from  :
+:: 7am to 6pm. The program is set to lock the screen and turn the    : 
+:: display off at 5pm daily. Computer will shutdown on Fri at 7pm.   :
+::                                                                   :
+:: Created By: Johnny Duncan										 :
+:: Last Modified: 6/19/18											 :
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 set flag=0
 set lockscreenflag=0
 set shutdownflag=0
 timeout /t 60
 
+:: checks day of the week and time 
 :begin
 set "DAY=%date:~0,3%" 
 set "hour=%time:~0,2%"
@@ -14,15 +21,18 @@ set "minute=%time:~3,2%"
 set "second=%time:~6,2%"
 echo %DAY% %hour% %minute% %second% 
 
+:: if between 7am and 5pm and first time call runit
 if %hour% geq 7 if %hour% leq 17 if %flag% equ 0 (goto runit)
+:: 
 if %hour%==17 if %lockscreenflag%==0 (goto lock)
+
 if %hour% geq 18 if %hour% lss 7 if %flag% equ 1 (goto killit)
 if %DAY%==Fri if %hour%==19 (goto shutDownNow)
 
 goto cont
 
 :runit
-echo entered runit %time%
+echo %time% entered runit
 start javaw -jar redlionexec.jar
 set flag=1
 set lockscreenflag=0
@@ -30,14 +40,16 @@ timeout /t 60
 goto begin
 
 :lock
-echo entered lock %time%
+echo %time% entered lock 
 lockscreenflag=1
+cd "C:\Program Files\NirCmd\"
+start nircmd.exe cmdwait 5000 monitor off
 rundll32.exe user32.dll, LockWorkStation
 timeout /t 60
 goto begin
 
 :killit
-echo entered killit %time%
+echo %time% entered killit
 taskkill /im chromedriver.exe
 taskkill /im javaw.exe
 set flag=0
@@ -45,12 +57,12 @@ timout /t 60
 goto begin
 
 :cont
-echo entered cont %time%
+echo %time% entered cont 
 timeout /t 60
 goto begin
 
 :shutDownNow
-echo turning off now
+echo %time% turning off now 
 shutdown /s /f
 
 exit
--- test.bat
+++ test.bat
@@ -1,6 +1,6 @@
 @echo off 
-timeout /t 30
-
+timeout /t 5
+cd "C:\Program Files\NirCmd\"
+start nircmd.exe cmdwait 5000 monitor off
 rundll32.exe user32.dll, LockWorkStation
-
 exit
